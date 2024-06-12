a3a5197ba8d91faafcf570b34713e317b2b62edb
Veselin Asenov <batveeeees@gmail.com>
2024-06-12 16:20:35 +0100

    fix issues in search

--- app/search/page.js
+++ app/search/page.js
@@ -1,5 +1,6 @@
+
 'use client';
-import { useEffect, useState } from "react";
+import { useEffect, useState, use } from "react";
 import Header from "../Component/Header";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebaseConfig";
@@ -19,30 +20,31 @@ export default function Search(){
 
         const data = [];
         docsSnap.forEach((doc) => {
-            data.push({id: doc.id, ...doc.data()});
-        });
+            data.push({id:doc.id,...doc.data()});
+        })
 
         setAllProducts(data);
         setFilteredProducts(data);
     }
 
-    useEffect(() => {
+    useEffect (()=> {
         getProductsData();
-    }, []); // dependency
+    }, []); //dependancy 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const filtered = allProducts.filter((product) => 
             product.brand.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            product.name.toLowerCase().includes(searchQuery.toLowerCase()) || 
-            product.type.toLowerCase().includes(searchQuery.toLowerCase())
+            product.name.toLowerCase().includes(searchQuery.toLocaleLowerCase()) || 
+            product.type.toLowerCase().includes(searchQuery.toLocaleLowerCase())  
         );
         setFilteredProducts(filtered);
     };
 
     return(
         <>
-            <hr />
+        
+        <hr />
             <h1 className="display-4 fw-normal">Search Product</h1>
             <form onSubmit={handleSubmit}>
                 <div className="row">
@@ -57,23 +59,13 @@ export default function Search(){
                 </div>
             </form>
 
-            <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1" key={cof.id}>
+            <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1">
                 {
-                    filteredProducts.map((cof) => (
-                        <ImgCard2
-                            values={{ 
-                                id: cof.id, 
-                                brand: cof.brand, 
-                                name: cof.name, 
-                                description: cof.description, 
-                                pType: cof.pType, 
-                                weight: cof.weight, 
-                                type: cof.type, 
-                                price: parseFloat(cof.price).toFixed(2), 
-                                roast: cof.roast, 
-                                logo: cof.logo 
-                            }} 
-                        />
+                    filteredProducts.map((cof, index) => (
+                    
+                        <div className="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1">
+                        <ImgCard2 key={index} values={{id:cof.id, brand:cof.brand, name:cof.name, description:cof.description, pType:cof.pType, weight:cof.weight, type:cof.type, price: parseFloat(cof.price).toFixed(2), roast:cof.roast, logo:cof.logo}} />
+                     </div>
                     ))
                 }
             </div>
